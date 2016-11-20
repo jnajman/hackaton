6689078ad4d1a1f33267c497f3b55807a706f3f7
jnajman <najman.honza@gmail.com>
2016-11-20 10:48:42 +0100

    web2

---
+++ hackaton_web/hack.engeto.com/tasks/anagram_skeleton.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# define a function to load the dictionary to internal structure
+# we will load it from external file
+def load_dict():
+    pass
+
+# process the input word we're working with
+
+# logic behind the anagram program
+# ideal case - work with the internal structure (array) with all
+# words from the dictionary and try to find proper letters in those words
+# it is up to you how you'll handle this area, try to figure this out
+
+# print the requested anagrams
---
+++ hackaton_web/hack.engeto.com/tasks/hangman_skeleton.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# define a function for generating a random word
+def choose_word():
+    pass
+
+word = choose_word()
+
+# main loop
+while True:
+    # print out current state of the game
+    # for example - We're guessing the word: _ _ _ _ _ _ _ _ _ _
+
+    # let's get a letter from the user
+    letter = input("Guess the letter: ")
+
+    # make sure the letter is only one character
+
+    # print the letter to see if everything is ok by now
+    print("You guessed", letter)
+
+    # determine if the letter is in the word
+
+    # check if the word is not guessed in its entirety
